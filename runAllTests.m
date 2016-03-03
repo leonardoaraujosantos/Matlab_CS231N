a1c0c6a4013879c2c8a66ed76b47d407b4714737
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2016-03-03 14:33:41 +0000

    Adding test TAP protocol

--- runAllTests.m
+++ runAllTests.m
@@ -1,11 +1,28 @@
+%% Script to run all tests (Must be called from console)
+%  cd /home/leo/work/Matlab_CS231N
+% /usr/local/bin/matlab -nodisplay -r runAllTests
+
 import matlab.unittest.TestSuite;
+import matlab.unittest.TestRunner;
+import matlab.unittest.plugins.TAPPlugin;
+import matlab.unittest.plugins.ToFile;
 
 try
-    suite = TestSuite.fromPackage('testcases','IncludingSubpackages',true);
-    results = run(suite);
+    %suite = TestSuite.fromPackage('tests','IncludingSubpackages',true);
+    % Run all Tests from folder tests
+    suite = TestSuite.fromFolder('tests');
+    % Create a typical runner with text output
+    runner = TestRunner.withTextOutput();
+    % Add the TAP plugin and direct its output to a file
+    tapFile = fullfile(getenv('WORKSPACE'), 'testResults.tap');
+    runner.addPlugin(TAPPlugin.producingOriginalFormat(ToFile(tapFile)));
+    % Run the tests
+    results = runner.run(suite);
     display(results);
 catch e
     disp(getReport(e,'extended'));
+    % Exit on any error
     exit(1);
 end
+% Exit on completion
 exit;

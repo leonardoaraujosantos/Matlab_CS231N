68e58c9363be235eeedf0f3c7f2a86c4d2a0f16e
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2016-03-20 12:42:38 +0000

    Use round to help visualize results

--- tests/DeepNeuralNetwork_simple_test.m
+++ tests/DeepNeuralNetwork_simple_test.m
@@ -23,13 +23,13 @@ layers.getLayer(1).weights = [-30; 20; 20];
 
 Xt = [0 0; 0 1; 1 0; 1 1];
 [maxscore, scores, predictTime] = nn.predict(Xt(1,:));
-fprintf('%d AND %d = %d\n',Xt(1,1), Xt(1,2), scores);
+fprintf('%d AND %d = %d\n',Xt(1,1), Xt(1,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(2,:));
-fprintf('%d AND %d = %d\n',Xt(2,1), Xt(2,2), scores);
+fprintf('%d AND %d = %d\n',Xt(2,1), Xt(2,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(3,:));
-fprintf('%d AND %d = %d\n',Xt(3,1), Xt(3,2), scores);
+fprintf('%d AND %d = %d\n',Xt(3,1), Xt(3,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(4,:));
-fprintf('%d AND %d = %d\n',Xt(4,1), Xt(4,2), scores);
+fprintf('%d AND %d = %d\n',Xt(4,1), Xt(4,2), round(scores));
 
 %% Test 2: Simple Perceptron OR
 % Create the DNN strucutre and train with Gradient Descent
@@ -51,13 +51,13 @@ layers.getLayer(1).weights = [-10; 20; 20];
 
 Xt = [0 0; 0 1; 1 0; 1 1];
 [maxscore, scores, predictTime] = nn.predict(Xt(1,:));
-fprintf('%d OR %d = %d\n',Xt(1,1), Xt(1,2), scores);
+fprintf('%d OR %d = %d\n',Xt(1,1), Xt(1,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(2,:));
-fprintf('%d OR %d = %d\n',Xt(2,1), Xt(2,2), scores);
+fprintf('%d OR %d = %d\n',Xt(2,1), Xt(2,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(3,:));
-fprintf('%d OR %d = %d\n',Xt(3,1), Xt(3,2), scores);
+fprintf('%d OR %d = %d\n',Xt(3,1), Xt(3,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(4,:));
-fprintf('%d OR %d = %d\n',Xt(4,1), Xt(4,2), scores);
+fprintf('%d OR %d = %d\n',Xt(4,1), Xt(4,2), round(scores));
 
 %% Test 3: Simple Multilayer layers creation
 % Create the DNN strucutre and train with Gradient Descent
@@ -82,12 +82,12 @@ layers.getLayer(2).weights = [-10; 20; 20];
 
 Xt = [0 0; 0 1; 1 0; 1 1];
 [maxscore, scores, predictTime] = nn.predict(Xt(1,:));
-fprintf('%d XOR %d = %d\n',Xt(1,1), Xt(1,2), scores);
+fprintf('%d XOR %d = %d\n',Xt(1,1), Xt(1,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(2,:));
-fprintf('%d XOR %d = %d\n',Xt(2,1), Xt(2,2), scores);
+fprintf('%d XOR %d = %d\n',Xt(2,1), Xt(2,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(3,:));
-fprintf('%d XOR %d = %d\n',Xt(3,1), Xt(3,2), scores);
+fprintf('%d XOR %d = %d\n',Xt(3,1), Xt(3,2), round(scores));
 [maxscore, scores, predictTime] = nn.predict(Xt(4,:));
-fprintf('%d XOR %d = %d\n',Xt(4,1), Xt(4,2), scores);
+fprintf('%d XOR %d = %d\n',Xt(4,1), Xt(4,2), round(scores));
 
 

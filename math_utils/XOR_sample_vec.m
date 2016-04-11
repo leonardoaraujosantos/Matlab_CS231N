c887c5c003d453c58d55c105a5ef82b06e0954c1
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2016-04-12 00:47:15 +0100

    Working on backpropagation
    Working vectorized version (simple XOR)
    Include on main class

---
+++ math_utils/XOR_sample_vec.m
@@ -0,0 +1,79 @@
+%% Nice XOR example on matlab based on this tutorial
+% http://matlabgeeks.com/tips-tutorials/neural-networks-a-multilayer-perceptron-in-matlab/
+% http://www.holehouse.org/mlclass/09_Neural_Networks_Learning.html
+% https://github.com/rasmusbergpalm/DeepLearnToolbox
+% We have here a 3 layer network (1-Input, 2-Hidden, 3-Output)
+
+% Sigmoid and dSigmoid functions
+sigmoid = @(x) 1.0 ./ ( 1.0 + exp(-x) );
+dsigmoid = @(x) sigmoid(x) .* ( 1 - sigmoid(x) );
+
+num_layers = 3;
+% XOR input for x1 and x2
+X = [0 0; 0 1; 1 0; 1 1];
+% Desired output of XOR
+Y_train = [0;1;1;1];
+% Learning coefficient
+learnRate = 0.1;
+% Number of learning iterations
+epochs = 5000;
+% Calculate weights randomly using seed.
+INIT_EPISLON = 0.1;
+W1 = rand(2,3) * (2*INIT_EPISLON) - INIT_EPISLON;
+W2 = rand(1,3) * (2*INIT_EPISLON) - INIT_EPISLON;
+
+%% Training
+for i = 1:epochs    
+    sizeTraining = length (X(:,1));    
+    complete_delta_1 = 0;
+    complete_delta_2 = 0;
+    for j = 1:sizeTraining        
+        %% Forward pass
+        % First Activation (Input-->Hidden)        
+        A1 = [ones(sizeTraining, 1) X];
+        Z2 = A1 * W1';
+        A2 = sigmoid(Z2);                
+        
+        % Second Activation (Hidden-->Output)
+        A2=[ones(sizeTraining, 1) A2];
+        Z3 = A2 * W2';
+        A3 = sigmoid(Z3);        
+                        
+        %% Backpropagation
+        % Find the partial derivative of the cost function related to all
+        % weights on the neural network (on our case 9 weights)
+        
+        % For output layer: (Why different tutorials have differ here?)       
+        % delta = (1-actual output)*(desired output - actual output)        
+        %delta_out_layer = (1-A3).*(Y_train-A3); % Other
+        %delta_out_layer = (Y_train-A3); % Andrew Ng        
+        delta_output = (A3-Y_train); % Andrew Ng
+        
+        % For Hidden layer        
+        Z2=[ones(sizeTraining,1) Z2];
+        delta_hidden=delta_output*W2.*dsigmoid(Z2);
+        
+        % Calculate complete delta for every weight (Testing....)  
+        delta_hidden=delta_hidden(:,2:end);
+        complete_delta_1 = complete_delta_1 + (delta_hidden'*A1);
+        complete_delta_2 = complete_delta_2 + (delta_output'*A2);                
+        
+        % Stochastic Gradient descent Update at every new input....
+        % Increment or decrement depending on delta_output sign
+        W1 = W1 - learnRate*complete_delta_1;
+        W2 = W2 - learnRate*complete_delta_2;        
+    end    
+    % Gradient descent Update after all training set deltas are calculated
+    % Increment or decrement depending on delta_output sign
+    %W1 = W1 - learnRate*complete_delta_1;
+    %W2 = W2 - learnRate*complete_delta_2;        
+end
+
+fprintf('Outputs\n');
+disp(round(A3));
+
+fprintf('\nW1\n');
+disp(W1);
+
+fprintf('\nW2\n');
+disp(W2);

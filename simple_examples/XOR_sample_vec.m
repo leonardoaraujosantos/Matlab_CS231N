e0ecfec9c461ba53aa1b8e7bc1a62af4844f46a3
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2016-04-15 11:44:23 +0100

    Adding comments and solving some small bugs. Also add regularization to the gradient checking part

--- simple_examples/XOR_sample_vec.m
+++ simple_examples/XOR_sample_vec.m
@@ -31,6 +31,7 @@
 X = [0 0; 0 1; 1 0; 1 1];
 % Desired output of XOR
 Y_train = [0;1;1;0];
+sizeTraining = size(X,1);
 
 %% Define the sigmoid and dsigmoid
 % Define the sigmoid (logistic) function and it's first derivative
@@ -40,10 +41,23 @@ dsigmoid = @(x) sigmoid(x) .* ( 1 - sigmoid(x) );
 %% Initialization of meta parameters
 % Learning coefficient
 learnRate = 2; % Start to oscilate with 15
-regularization = 0.00;
+regularization = 0.01;
 % Number of learning iterations
 epochs = 2000;
 smallStep = 0.0001;
+
+%% Define neural network structure
+% More neurons means more local minimas, which means easier training and
+% even if you get stuck on a local minima it's error will be close to the
+% global minima (For large deep networks)
+%
+% Who is afraid of non convex loss-functions?
+%
+% http://videolectures.net/eml07_lecun_wia/
+%
+input_layer_size = 2;
+hidden_layer_size = 2;
+output_layer_size = 1;
 
 %% Cost function definition
 % On this case we will use the Cross entropy cost(or loss) function, the
@@ -54,33 +68,26 @@ J_vec = zeros(1,epochs);
 lossFunction = CrossEntropy();
 
 %% Weights random initialization
-% This changes a lot the training performance! Some links:
+% Initialize all the weights(parameters) of the neural network, layer by
+% layer, the rule to create them is W(layer)=[next_layer X prev_layer+1].
+% Where W1 is the layer that map the activations of the input layer to the
+% hidden layer and W2 map the activations of the hidden layer to the output
 %
-% http://www.cs.toronto.edu/~hinton/absps/momentum.pdf
+% The way that the random values are distributed changes a lot the training
+% performance, check chapter 3 on coursera (Weight initialization)
 %
 % http://neuralnetworksanddeeplearning.com/chap3.html#weight_initialization
 %
-% Here every weight row represent one neuron connection, ex weights(1,:)
-% means all connections of the first neuron
-%
-% Put random number generator on state 0
-%rand('state',0);
+%rand('state',0); % Put random number generator on state 0
 % Same thing but new syntax
 rng(0,'v5uniform');
 INIT_EPISLON = 0.8;
-W1 = rand(2,3) * (2*INIT_EPISLON) - INIT_EPISLON;
-W2 = rand(1,3) * (2*INIT_EPISLON) - INIT_EPISLON;
+W1 = rand(hidden_layer_size,input_layer_size+1) * (2*INIT_EPISLON) - INIT_EPISLON;
+W2 = rand(output_layer_size,hidden_layer_size+1) * (2*INIT_EPISLON) - INIT_EPISLON;
 % Override manually to debug both vectorized and non vectorized
 % implementation
 %W1 = [-0.7690    0.6881   -0.2164; -0.0963    0.2379   -0.1385];
 %W2 = [-0.1433   -0.4840   -0.6903];
-
-% More neurons means more local minimas, which means easier training if you
-% dont get stuck on a local minima
-% Who is afraid of non convex loss-functions?
-% http://videolectures.net/eml07_lecun_wia/
-%W1 = rand(15,3) * (2*INIT_EPISLON) - INIT_EPISLON;
-%W2 = rand(1,16) * (2*INIT_EPISLON) - INIT_EPISLON;
 
 %% Training
 for i = 1:epochs
@@ -106,10 +113,7 @@ for i = 1:epochs
     %
     Thetas = [W1(:) ; W2(:)];
     numgrad = zeros(size(Thetas));
-    perturb = zeros(size(Thetas));
-    hidden_layer_size = 2;
-    input_layer_size = 2;
-    output_layer_size = 1;
+    perturb = zeros(size(Thetas));    
     for p = 1:numel(Thetas)
         % Set perturbation vector
         perturb(p) = smallStep;
@@ -124,6 +128,7 @@ for i = 1:epochs
         nW2 = reshape(ThetasLoss1((1 + (hidden_layer_size * ...
             (input_layer_size + 1))):end), output_layer_size, ...
             (hidden_layer_size + 1));
+        p_loss1 = sum(sum(nW1(:, 2:end).^2, 2))+sum(sum(nW2(:, 2:end).^2, 2));        
         
         % Forward prop
         A1 = [ones(sizeTraining, 1) X];
@@ -142,6 +147,8 @@ for i = 1:epochs
         nW2 = reshape(ThetasLoss2((1 + (hidden_layer_size * ...
             (input_layer_size + 1))):end), output_layer_size, ...
             (hidden_layer_size + 1));
+        p_loss2 = sum(sum(nW1(:, 2:end).^2, 2))+sum(sum(nW2(:, 2:end).^2, 2));        
+        
         % Forward prop
         A1 = [ones(sizeTraining, 1) X];
         Z2 = A1 * nW1';
@@ -151,9 +158,9 @@ for i = 1:epochs
         A3 = sigmoid(Z3);
         h2 = A3;
         
-        % Calculate both losses...
-        loss1 = CrossEntrInst.getLoss(h1,Y_train);
-        loss2 = CrossEntrInst.getLoss(h2,Y_train);
+        % Calculate both losses...        
+        loss1 = lossFunction.getLoss(h1,Y_train) + regularization*p_loss1/(2*sizeTraining);
+        loss2 = lossFunction.getLoss(h2,Y_train) + regularization*p_loss2/(2*sizeTraining);
         
         % Compute Numerical Gradient
         numgrad(p) = (loss2 - loss1) / (2*smallStep);
@@ -171,6 +178,9 @@ for i = 1:epochs
     % weights on the neural network (on our case 9 weights)
     
     %%% Forward pass
+    % Move from left(input) layer to the right (output), observe that every
+    % previous activation get a extra collumn of ones (Include Bias)
+    %
     % First Activation (Input-->Hidden)
     A1 = [ones(sizeTraining, 1) X];
     Z2 = A1 * W1';
@@ -183,15 +193,21 @@ for i = 1:epochs
     h = A3;
     
     %%% Backward pass
-    % For output layer: (Why different tutorials have differ here?)
-    % delta = (1-actual output)*(desired output - actual output)
+    % Move from right(output) layer to the left (input), actually stopping
+    % on the last hidden layer before the input. Here we want to calculate
+    % the error of every layer (desired - actual). The trap here is that
+    % you calculate the output layer differenly than the input layers
+    %
+    % Output layer: (Why different tutorials have differ here?)    
     %delta_out_layer = A3.*(1-A3).*(A3-Y_train); % Other
     %delta_out_layer = (Y_train-A3); % Andrew Ng
     delta_output = (A3-Y_train); % Andrew Ng (Invert weight update signal)
     
-    % For Hidden layer
-    Z2=[ones(sizeTraining,1) Z2];
+    % Hidden layer, same idea of adding collumn of ones to include bias
+    Z2=[ones(sizeTraining,1) Z2];    
+    % Observe that we use the delta of the next layer
     delta_hidden=delta_output*W2.*dsigmoid(Z2);
+    
     % Take out first column (bias column), to force the complete delta
     % to have the same size of it's respective weight
     delta_hidden=delta_hidden(:,2:end);
@@ -204,7 +220,7 @@ for i = 1:epochs
     % avoiding regularizing the bias term by substituting the first col of
     % weights with zeros
     p1 = ((regularization/sizeTraining)* [zeros(size(W1, 1), 1) W1(:, 2:end)]);
-    p2 = ((regularization/sizeTraining)* [zeros(size(W2, 1), 1) W1(2, 2:end)]);
+    p2 = ((regularization/sizeTraining)* [zeros(size(W2, 1), 1) W2(:, 2:end)]);
     D1 = (complete_delta_1 ./ sizeTraining) + p1;
     D2 = (complete_delta_2 ./ sizeTraining) + p2;
     
@@ -215,7 +231,7 @@ for i = 1:epochs
     errorBackPropD1 = sum(sum(abs(D1 - numDeltaW1)));
     errorBackPropD2 = sum(sum(abs(D2 - numDeltaW2)));
     % Stop if backpropagation error bigger than 0.0001
-    if (errorBackPropD1 > 0.001) || (errorBackPropD2 > 0.001)
+    if (errorBackPropD1 > 0.0001) || (errorBackPropD2 > 0.0001)
         fprintf('Backpropagation error %d %d\n',errorBackPropD1,errorBackPropD2);
         pause;
     end
@@ -230,22 +246,27 @@ for i = 1:epochs
     W2 = W2 - learnRate*(D2);
     
     %%% Calculate cost
-    % After all calculations on the epoch calculate the cost function
-    % Calculate Cost function output
-    % Calculate p (used on regression)
+    % After all calculations on the epoch are finished, calculate the cost
+    % function between all your predictions (during) training and your
+    % actual desired training output. The cost is a number representing how
+    % well you are going on the training. For improving overfitting we also
+    % include a regularization term that will avoid your training weights
+    % get bigger values during training
+    %
+    % Calculate p (Regularization term)
     p = sum(sum(W1(:, 2:end).^2, 2))+sum(sum(W2(:, 2:end).^2, 2));
-    % calculate J
-    %J = sum(sum((-Y_train).*log(h) - (1-Y_train).*log(1-h), 2))/sizeTraining + regularization*p/(2*sizeTraining);
-    J = CrossEntrInst.getLoss(h,Y_train) + regularization*p/(2*sizeTraining);
+    % calculate Loss(or cost)    
+    J = lossFunction.getLoss(h,Y_train) + regularization*p/(2*sizeTraining);
+    
+    %%% Early stop
+    % Stop if error is smaller than 0.01
     J_vec(i) = J;
-    %     % Break if error is already low
-    %     if J < 0.08
-    %         break;
-    %     end
+    % Break if error is already low
+    if J < 0.01
+        J_vec = J_vec(1:i);
+        break;
+    end
 end
-
-fprintf('Outputs\n');
-disp(round(A3));
 
 %% Plot some information
 % Plot Prediction surface and Cost vs epoch curve
@@ -269,6 +290,14 @@ for row = [1:testOutRows]
         Z3 = A2 * W2';
         A3 = sigmoid(Z3);
         testOut(row, col) = A3;
+        
+        % Print XOR table
+        if isequal(test,[0 0]) || ...
+                isequal(test,[0 1]) || ...
+                isequal(test,[1 0]) || ...
+                isequal(test,[1 1])
+           fprintf('%d XOR %d ==> %d\n',test(1),test(2),round(A3)); 
+        end
     end
 end
 figure(2);

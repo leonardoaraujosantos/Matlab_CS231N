1e998a5cfd254e5e136780602b5944d304740fbb
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2016-07-13 17:20:57 +0100

    Commenting RL Policy gradient example

--- python_reference_code/book_ai/policy_gradient_pong.py
+++ python_reference_code/book_ai/policy_gradient_pong.py
@@ -34,9 +34,9 @@ gamma = 0.99
 # decay factor for RMSProp leaky sum of grad^2
 decay_rate = 0.99
 # resume from previous checkpoint?
-resume = False
+resume = True
 # Put to true to show game screen
-showGame = True
+showGame = False
 # Save W1 weights
 showWeights = False
 # Save input images
@@ -49,6 +49,7 @@ saveInputImages = False
 D = 80 * 80 # input dimensionality: 80x80 grid
 if resume:
     # Load saved model
+    print('Loading pre-trained model')
     model = pickle.load(open('save.p', 'rb'))
 
     if showWeights == True:
@@ -57,6 +58,7 @@ if resume:
 else:
     model = {}
     # Do "Xavier" initialization W1=[200x640] W2=[200]
+    # Our network have 200(H) neurons on the input layer and 1 output neuron
     model['W1'] = np.random.randn(H,D) / np.sqrt(D)
     model['W2'] = np.random.randn(H) / np.sqrt(H)
 
@@ -93,18 +95,18 @@ def discount_rewards(r):
 
 # Normal neural network forward propagation (Policy network)
 def policy_forward(x):
-    # X,W1 dot product (Matrix multiplication)    
+    # X,W1 dot product (Matrix multiplication)
     h = np.dot(model['W1'], x)
-    
+
     # Apply ReLU nonlinearity
     h[h<0] = 0
-    
-    # h(after relu),W2 dot product (Matrix multiplication)    
+
+    # h(after relu),W2 dot product (Matrix multiplication)
     logp = np.dot(model['W2'], h)
-    
+
     # get probability by squashing result
     p = sigmoid(logp)
-    
+
     # return probability of taking action 2, and hidden state
     return p, h
 
@@ -112,14 +114,14 @@ def policy_forward(x):
 def policy_backward(previous_w1_h, previous_x, dout):
     # Backward fully conected layer (W2)
     dW2 = np.dot(previous_w1_h.T, dout).ravel()
-    
+
     # Backward Relu
     dh = np.outer(dout, model['W2'])
-    dh[previous_w1_h <= 0] = 0 
-    
+    dh[previous_w1_h <= 0] = 0
+
     # Backward fully conected layer (W1)
     dW1 = np.dot(dh.T, previous_x)
-    
+
     # Return gradient for W1 and W2
     return {'W1':dW1, 'W2':dW2}
 
@@ -129,7 +131,7 @@ observation = gameEnviroment.reset()
 
 # Initialize variables
 prev_x = None
-xs,hs,dlogps,drs = [],[],[],[]
+inputs_vector,hidden_vector,dlogps,reward_vector = [],[],[],[]
 running_reward = None
 reward_sum = 0
 episode_number = 0
@@ -157,74 +159,80 @@ while True:
 
     # forward the policy network with input image
     # Return action probability and hidden state activations
-    aprob, h = policy_forward(x)
+    action_UP_prob, h = policy_forward(x)
 
     # Action will be the atari joystick command check the available commands
     # here:
     # https://github.com/openai/gym/blob/master/gym/envs/atari/atari_env.py
     # np.random.uniform() return a non-gaussian number from 0..1
-    action = 2 if np.random.uniform() < aprob else 5 # roll the dice!        
+    # Here we sample our action(UP/DOWN) from our probability distribution
+    action = 2 if np.random.uniform() < action_UP_prob else 5
 
     # Save input and hidden(W1) activations (needed later for backprop)
-    xs.append(x) # input
-    hs.append(h) # hidden state
-    
-    # y=1 if action==UP else 0 (Create label data)
+    inputs_vector.append(x) # input
+    hidden_vector.append(h) # hidden state
+
+    # y=1 if action==UP else 0 (Create label data), at this point we don't
+    # know yet if the action was good/bad
     y = 1 if action == 2 else 0 # a "fake label"
 
     # grad that encourages the action that was taken to be
     # taken see:
     # http://cs231n.github.io/neural-networks-2/#losses
-    # y - aprob (Think as a network error)
-    dlogps.append(y - aprob)
+    # We don't know yet if this action was good... (We need to wait the reward)
+    dlogps.append(y - action_UP_prob)
 
     # step the environment and get new measurements
     # Reward will be +1 if ball passed oponent size, -1 if passed your side
     observation, reward, done, info = gameEnviroment.step(action)
     reward_sum += reward
-        
+
     # Save reward (call after "step" to get the reward of previous action)
-    drs.append(reward)
+    reward_vector.append(reward)
 
     # Game-over episode finished (Did we loose or win)
     # One complete episode means that the ball passed at least 20 times
     if done:
         episode_number += 1
-        
+
         # stack together all inputs, hidden states, action gradients,
         # and rewards for this episode vertically (row-wise)
-        batch_x = np.vstack(xs)
-        eph = np.vstack(hs)
-        epdlogp = np.vstack(dlogps)
-        epr = np.vstack(drs)
-        xs,hs,dlogps,drs = [],[],[],[] # reset array memory
+        batch_x = np.vstack(inputs_vector)
+        batch_hidden_act = np.vstack(hidden_vector)
+        batch_prob_grad = np.vstack(dlogps)
+        batch_rewards = np.vstack(reward_vector)
+        # reset arrays for next episodes
+        inputs_vector,hidden_vector,dlogps,reward_vector = [],[],[],[]
 
-        # compute the discounted reward backwards through time, this is done 
+        # compute the discounted reward backwards through time, this is done
         # to have preference to faster rewards, with this long rewards has less
         # utility...
-        discounted_epr = discount_rewards(epr)
-        
+        discounted_batch_rewards = discount_rewards(batch_rewards)
+
         # standardize the rewards to be unit normal
         # (helps control the gradient estimator variance)
-        discounted_epr -= np.mean(discounted_epr)
-        discounted_epr /= np.std(discounted_epr)
+        discounted_batch_rewards -= np.mean(discounted_batch_rewards)
+        discounted_batch_rewards /= np.std(discounted_batch_rewards)
 
-        # modulate the gradient with advantage (PG magic happens right here.)
-        epdlogp *= discounted_epr
+        # Multiply the action gradient (1) by some possible reward (0,+1,-1),
+        # this will modulate the gradient and make the current choice more/less
+        # likely to happen on the future
+        batch_prob_grad *= discounted_batch_rewards
 
         # Get network gradients (Backpropagation)
-        grad = policy_backward(eph, batch_x, epdlogp)
+        grad = policy_backward(batch_hidden_act, batch_x, batch_prob_grad)
         for k in model:
             grad_buffer[k] += grad[k] # accumulate grad over batch
 
         # perform rmsprop parameter update every batch_size episodes
         if episode_number % batch_size == 0:
-          for k,v in model.iteritems():
-              g = grad_buffer[k] # gradient
-              rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + \
-                  (1 - decay_rate) * g**2
-              model[k] += learn_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
-              grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
+            print('********** Update gradients with rmsprop **************')
+            for k,v in model.iteritems():
+                g = grad_buffer[k] # gradient
+                rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + \
+                    (1 - decay_rate) * g**2
+                model[k] += learn_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
+                grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
 
         # boring book-keeping
         running_reward = reward_sum if running_reward is None else \
